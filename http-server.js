3240a1eae8d67c27f311747a8f7bd85d7dc552f2
Rebeca Araujo <104568778+ReisLimaaaa@users.noreply.github.com>
2023-06-06 18:16:03 +0000

    alegria

--- http-server.js
+++ http-server.js
@@ -4,11 +4,31 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-app.use("/qhrssao", (req, res) => {
+app.use("/create", (req, res) => {
     const { file, texto } = req.query
     fs.writeFileSync(file, texto)
-    fs.deleteFileSync()
     res.send(new Date())
+})
+
+app.use("/read", (req,res) => {
+    const { file } = req.query
+    const teste = fs.readFileSync(file, 'utf8')
+
+    res.send(teste)
+})
+
+app.use("/update", (req,res) => {
+    const { file, texto } = req.query
+    const update = fs.appendFileSync(file, texto)
+
+    res.send(update)
+})
+
+app.use("/delete", (req,res) => {
+    const { file, query } = req.query
+    const rm = fs.rmSync(file)
+
+    res.send(rm)
 })
 
 app.listen(3000, () => console.log("Servidor rodando !"))
